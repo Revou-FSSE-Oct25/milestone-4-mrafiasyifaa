3a01f4e74e1e6c0f717c5c6f9dbddb05f1817811
Rafi A Syifaa <mrafiasyifaa@gmail.com>
2026-04-15 15:49:04 +0700

    fix: Schema.Prisma 7 docs changed everything

--- revobank/src/app.module.ts
+++ revobank/src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { AccountController } from './account/account.controller';
-import { TransactionController } from './transaction/transaction.controller';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { AccountModule } from './account/account.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController, AccountController, TransactionController],
-  providers: [AppService, PrismaService],
+  imports: [PrismaModule, UserModule, AccountModule, TransactionModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- revobank/src/prisma/prisma.service.ts
+++ revobank/src/prisma/prisma.service.ts
@@ -1,13 +1,29 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client/extension';
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "generated/prisma/client";
+
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService implements OnModuleInit, OnModuleDestroy{
+    private readonly prisma: PrismaClient
+    
+    constructor(){
+        const adapter = new PrismaPg({
+            connectionString: process.env.DATABASE_URL
+        })
+        this.prisma = new PrismaClient({adapter})
+    }
+
+    get db(){
+        return this.prisma
+    }
+
     async onModuleInit() {
-        await this.$connect();
+        await this.prisma.$connect()
     }
 
     async onModuleDestroy() {
-        await this.$disconnect();
+        await this.prisma.$disconnect()
     }
+
 }

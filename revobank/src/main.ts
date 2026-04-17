31822fdce3625a8ca26f35e88bff363763d2c65a
Rafi A Syifaa <mrafiasyifaa@gmail.com>
2026-04-17 14:06:23 +0700

    docs: Adding swagger

--- revobank/src/main.ts
+++ revobank/src/main.ts
@@ -3,11 +3,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalFilters(new HttpExceptionFilter())
+
+  const config = new DocumentBuilder()
+    .setTitle('Revobank API')
+    .setDescription('API Documentation for Milestone 4')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs-v1', app, document)
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
